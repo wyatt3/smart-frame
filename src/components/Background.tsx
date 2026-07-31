f0097cd4ea50d3eacd0d7f371ef31d05e29cf169
Wyatt Johnson <wyatt.j1834@gmail.com>
2026-07-31 08:41:07 -0600

    Go through all images before repeating, check for new images  on every change

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,22 +1,36 @@
 import { useState, useEffect } from "react";
 import styles from "./Background.module.css";
-const INTERVAL_MS = 360_000;
+const INTERVAL_MS = 120_000;
 
 function Background() {
   const [imgUrl, setImgUrl] = useState("");
 
   useEffect(() => {
-    function pickRandom() {
-      window.ipcRenderer.invoke("get-images").then((images: string[]) => {
-        if (images.length > 0) {
-          const random = images[Math.floor(Math.random() * images.length)];
-          setImgUrl(`/img/${random}`);
+    var currentImage = 0;
+    var images: string[] = [];
+
+    function shuffleArray(array: string[]) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+
+    function checkImages() {
+      window.ipcRenderer.invoke("get-images").then((result: string[]) => {
+        const same = images.length === result.length && images.every((img) => result.includes(img));
+        if (same) {
+          setImgUrl("/img/" + images[currentImage++ % result.length]);
+        } else {
+          shuffleArray(result);
+          images = result;
+          setImgUrl("/img/" + images[currentImage++ % result.length]);
         }
       });
     }
 
-    pickRandom();
-    const id = setInterval(pickRandom, INTERVAL_MS);
+    checkImages();
+    const id = setInterval(checkImages, INTERVAL_MS);
     return () => clearInterval(id);
   }, []);
 
